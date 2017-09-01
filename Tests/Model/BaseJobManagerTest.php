6305003353b5c0cee8d48a9f400d5b571ec665ec
Matthew J. Mucklo <mmucklo@gmail.com>
2017-09-01 09:22:51 -0700

    why do I need to lower the job count?

--- Tests/Model/BaseJobManagerTest.php
+++ Tests/Model/BaseJobManagerTest.php
@@ -87,7 +87,7 @@ abstract class BaseJobManagerTest extends TestCase
         echo "\n".static::class.": Testing Performance\n";
         flush();
         $start = microtime(true);
-        $jobsTotal = 1000;
+        $jobsTotal = 100;
         self::$jobManager->enableSorting = false;    // Ignore priority
 
         for ($i = 0; $i < $jobsTotal; ++$i) {
