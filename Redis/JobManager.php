436895fd171c3c903fe9ea5e227692e427dfe58d
Matthew J. Mucklo <mmucklo@gmail.com>
2017-11-11 14:12:26 -0800

    for SensioLabs Insight

--- Redis/JobManager.php
+++ Redis/JobManager.php
@@ -4,15 +4,15 @@ namespace Dtc\QueueBundle\Redis;
 
 use Dtc\QueueBundle\Model\PriorityJobManager;
 
-// For future
-//class JobManager extends PriorityJobManager {
-//
-//    private $redis;
-//
-//    public function setRedis(RedisInterface $redis) {
-//        $this->redis = $redis;
-//    }
-//
-//
-//
-//}
+/**
+ * For future implementation.
+ */
+abstract class JobManager extends PriorityJobManager
+{
+    private $redis;
+
+    public function setRedis(RedisInterface $redis)
+    {
+        $this->redis = $redis;
+    }
+}
