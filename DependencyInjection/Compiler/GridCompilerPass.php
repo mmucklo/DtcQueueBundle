48d412b371ab39092b382f9dd98ea7fb4e9b3e98
Matthew J. Mucklo <mmucklo@gmail.com>
2021-05-19 22:14:54 -0700

    Add StubLiveJobsGridSource files for an attempt at fixing issue #143

--- DependencyInjection/Compiler/GridCompilerPass.php
+++ DependencyInjection/Compiler/GridCompilerPass.php
@@ -12,9 +12,17 @@ class GridCompilerPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         if (!class_exists('Dtc\GridBundle\DtcGridBundle')) {
+            $container->getDefinition('dtc_queue.grid_source.jobs_waiting.odm')->setClass('Dtc\QueueBundle\ODM\StubLiveJobsGridSource');
+            $container->getDefinition('dtc_queue.grid_source.jobs_waiting.orm')->setClass('Dtc\QueueBundle\ORM\StubLiveJobsGridSource');
+            $container->getDefinition('dtc_queue.grid_source.jobs_running.odm')->setClass('Dtc\QueueBundle\ODM\StubLiveJobsGridSource');
+            $container->getDefinition('dtc_queue.grid_source.jobs_running.orm')->setClass('Dtc\QueueBundle\ORM\StubLiveJobsGridSource');
+
             return;
         }
-
+        $container->getDefinition('dtc_queue.grid_source.jobs_waiting.odm')->setClass('Dtc\QueueBundle\ODM\LiveJobsGridSource');
+        $container->getDefinition('dtc_queue.grid_source.jobs_waiting.orm')->setClass('Dtc\QueueBundle\ORM\LiveJobsGridSource');
+        $container->getDefinition('dtc_queue.grid_source.jobs_running.odm')->setClass('Dtc\QueueBundle\ODM\LiveJobsGridSource');
+        $container->getDefinition('dtc_queue.grid_source.jobs_running.orm')->setClass('Dtc\QueueBundle\ORM\LiveJobsGridSource');
         $defaultManagerType = $container->getParameter('dtc_queue.manager.job');
         $runManagerType = $container->getParameter($this->getRunManagerType($container));
         if ('orm' === $defaultManagerType || 'orm' === $runManagerType || 'odm' === $defaultManagerType || 'odm' === $runManagerType) {
